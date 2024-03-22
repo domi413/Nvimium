38e7d636b0bef9b39a7f4d2b0436cdb96eaaaab2
domi <152265924+domi413@users.noreply.github.com>
2024-03-22 16:44:46 +0100

    Add files via upload

---
+++ nvim/lua/domi/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+}
+-- return {
+-- 	"lukas-reineke/indent-blankline.nvim",
+-- 	config = function()
+-- 		vim.opt.list = true
+-- 		vim.opt.listchars:append("space:⋅") -- show tabs with spaces
+-- 		-- vim.opt.listchars:append("eol:↴") -- show line break
+--
+-- 	end,
+-- }
