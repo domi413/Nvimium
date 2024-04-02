277a7615b829db76e569f2787b1e68eda3d7b887
domi <152265924+domi413@users.noreply.github.com>
2024-04-02 09:15:43 +0200

    Add files via upload

---
+++ nvim/TODO.md
@@ -0,0 +1,23 @@
+# TODOs for Neovim
+
+## Toolbar
+
+1. Disable pinguin, encoding and box?
+2. enable indentation
+3. maybe add system monitor (as in VSCode)
+
+## Tabout
+
+- Apply tabout plugin
+
+## Theme
+
+- Save selected theme as default, so the next time nvim starts, the previous selected theme gets loaded
+
+## indentation
+
+- Fix indentation configs, so the spaces are visible as dots
+
+## Icons
+
+- Add a more beautiful icon pack
--- nvim/lua/domi/plugins/colorscheme.lua
+++ nvim/lua/domi/plugins/colorscheme.lua
@@ -2,19 +2,18 @@ return {
 	{
 		"rmehri01/onenord.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
+
+		-- Set default colorscheme
 		config = function()
-			-- load the colorscheme here
 			vim.cmd([[colorscheme tokyonight-night]])
 		end,
 	},
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
 		priority = 1000,
-		opts = {},
 	},
 	{
 		"navarasu/onedark.nvim",
-		priority = 100,
+		priority = 1000,
 	},
 }
