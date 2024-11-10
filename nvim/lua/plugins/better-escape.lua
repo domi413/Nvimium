9b27552d6fbbeec64a3019f3cd9bd58e91bff772
domi413 <dom.buehler@outlook.com>
2024-11-10 15:31:34 +0100

    change lualine delimiter dynamically and disable jj to leave insert mode

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -4,6 +4,12 @@ return {
 	config = function()
 		require("better_escape").setup({
 			mappings = {
+				i = {
+					j = {
+						j = false, -- disable jj to leave insert
+						k = "<ESC>",
+					},
+				},
 				t = { j = { false } }, --lazygit navigation fix
 				v = { j = { k = false } }, -- visual select fix
 				s = { j = { k = false } }, -- selection mode (snippets) fix
